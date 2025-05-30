d096ea915341252343479b55ec6c48bd805c7d1d
ante237 <anton.helmersson00@gmail.com>
2025-05-30 16:29:38 +0200

    First test case

--- test/unit/test_detect_duplicates.py
+++ test/unit/test_detect_duplicates.py
@@ -1,7 +1,32 @@
 import pytest
-
+from unittest.mock import patch
+from src.util.detector import detect_duplicates
 # develop your test cases here
 
 @pytest.mark.unit
-def test_detect_duplicates():
+@patch("src.util.detector.parse", return_value=[])
+def test_detect_duplicates_1():
+    
+    ret = detect_duplicates("")
+
+    assert ret == []
+
+@pytest.mark.unit
+def test_detect_duplicates_2():
+    assert True
+
+@pytest.mark.unit
+def test_detect_duplicates_3():
+    assert True
+
+@pytest.mark.unit
+def test_detect_duplicates_4():
+    assert True
+
+@pytest.mark.unit
+def test_detect_duplicates_5():
+    assert True
+
+@pytest.mark.unit
+def test_detect_duplicates_6():
     assert True
