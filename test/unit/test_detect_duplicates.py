b39429806f14a46cd2da1ab83af42212810bf6c2
ante237 <anton.helmersson00@gmail.com>
2025-05-30 16:46:26 +0200

    Finished unit tests

--- test/unit/test_detect_duplicates.py
+++ test/unit/test_detect_duplicates.py
@@ -6,29 +6,39 @@ from src.util.detector import detect_duplicates
 
 
 @pytest.mark.unit
+#Patch parse function to return correct value according to test. This is done for all functions
 @patch("src.util.detector.parse", return_value=[])
 def test_detect_duplicates_1(mock_parse):
-    
-    ret = detect_duplicates("")
+    with pytest.raises(ValueError):
+        detect_duplicates("")
 
+
+@pytest.mark.unit
+@patch("src.util.detector.parse", return_value=[{"key": "1", "doi": "testDOI1"}])
+def test_detect_duplicates_2(mock_parse):
+    with pytest.raises(ValueError):
+        detect_duplicates("")
+
+@pytest.mark.unit
+@patch("src.util.detector.parse", return_value=[{"key": "1", "doi": "testDOI1"}, {"key": "1", "doi": "testDOI1"}])
+def test_detect_duplicates_3(mock_parse):
+    ret = detect_duplicates("")
+    assert ret == [{"key": "1", "doi": "testDOI1"}]
+
+@pytest.mark.unit
+@patch("src.util.detector.parse", return_value=[{"key": "1", "doi": "testDOI1"}, {"key": "2", "doi": "testDOI2"}])
+def test_detect_duplicates_4(mock_parse):
+    ret = detect_duplicates("")
     assert ret == []
 
 @pytest.mark.unit
-def test_detect_duplicates_2():
-    assert True
+@patch("src.util.detector.parse", return_value=[{"key": "1"}, {"key": "1", "doi": "testDOI1"}])
+def test_detect_duplicates_5(mock_parse):
+    ret = detect_duplicates("")
+    assert ret == [{"key": "1", "doi": "testDOI1"}]
 
 @pytest.mark.unit
-def test_detect_duplicates_3():
-    assert True
-
-@pytest.mark.unit
-def test_detect_duplicates_4():
-    assert True
-
-@pytest.mark.unit
-def test_detect_duplicates_5():
-    assert True
-
-@pytest.mark.unit
-def test_detect_duplicates_6():
-    assert True
+@patch("src.util.detector.parse", return_value=[{"key": "1"}, {"key": "2", "doi": "testDOI2"}])
+def test_detect_duplicates_6(mock_parse):
+    ret = detect_duplicates("")
+    assert ret == []
